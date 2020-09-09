e555320b4899f788479569fe956870fae2fc7ff2
PatrykKarbowy <patryk.karbowy11@gmail.com>
2020-09-09 17:35:12 +0200

    Update

--- src/main/java/com/example/RestAPIApp/RestApiAppApplication.java
+++ src/main/java/com/example/RestAPIApp/RestApiAppApplication.java
@@ -2,12 +2,28 @@ package com.example.RestAPIApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestApiAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiAppApplication.class, args);
+	}
+	@Bean
+	public Docket get() {
+		return new Docket(DocumentationType.SWAGGER_12)
+				.select()
+				.paths(PathSelectors.ant("/api/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.example.RestAPIApp"))
+				.build();
 	}
 
 }
